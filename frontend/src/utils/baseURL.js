e67655439a3b6d4e60235dd6ee586629e5480c08
Harshit Gupta <jacksparrow53536@gmail.com>
2026-07-17 20:38:51 +0530

    Update production API URL

--- frontend/src/utils/baseURL.js
+++ frontend/src/utils/baseURL.js
@@ -1,5 +1,11 @@
 const getBaseUrl = () => {
-    return "http://localhost:8080"
+    return "https://book-store-mern-a8yp.onrender.com";
 }
 
 export default getBaseUrl;
+
+function getImgUrl(name) {
+    return new URL(`../assets/books/${name}`, import.meta.url);
+}
+
+export { getImgUrl };
